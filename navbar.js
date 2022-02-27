d8a7d122269f0450060a75b8c20519160bdd56ad
Tomas Sorgetti <tomassorgetti456@gmail.com>
2022-02-27 20:29:31 -0300

    cambios de lorem por texto, cambios generales, correccion de errores

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ var myNav = document.getElementById("navbar");
 
 window.onscroll = function() {
   "use strict";
-  if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+  if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
     myNav.classList.add("scroll");
   } else {
     myNav.classList.remove("scroll");
